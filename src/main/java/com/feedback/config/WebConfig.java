8201db5780669cb2713c077adaad0a2302389378
Ivan Godzynskyi <Ivan_Godzynskyi@epam.com>
2017-06-25 01:56:14 +0300

    Added ability to retrieve static resources.

--- src/main/java/com/feedback/config/WebConfig.java
+++ src/main/java/com/feedback/config/WebConfig.java
@@ -1,21 +1,26 @@
 package com.feedback.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @Import({PostgresDBConfig.class, AspectConfig.class})
 @ComponentScan("com.feedback")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pages/**").addResourceLocations("/WEB-INF/pages/");
+        registry.addResourceHandler("/html/**").addResourceLocations("/WEB-INF/html/");
+    }
 
 //    @Bean
 //    public InternalResourceViewResolver setupViewResolver() {
